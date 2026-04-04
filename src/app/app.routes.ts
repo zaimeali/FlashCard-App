c704b972196903db9297054255f54f80c27cedf7
zaimeali <zaimeali1997@gmail.com>
2026-04-04 00:00:17 -0500

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/auth/login/login';
+import { Home } from './components/home/home';
+import { Dashboard } from './components/dashboard/dashboard';
 
 export const routes: Routes = [
     {
         path: '',
         component: Login
+    },
+    {
+        path: 'home',
+        component: Home
+    },
+    {
+        path: 'dashboard',
+        component: Dashboard
     }
 ];
