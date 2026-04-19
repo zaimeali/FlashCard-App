f05fc1d8745bfb61aac9f3e5b9531469156ed8db
zaimeali <zaimeali1997@gmail.com>
2026-04-18 23:16:06 -0500

    Added Content for Terms and Privacy Policy page

--- src/app/components/terms-page/terms-page.ts
+++ src/app/components/terms-page/terms-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-terms-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './terms-page.html',
   styleUrl: './terms-page.scss',
 })
